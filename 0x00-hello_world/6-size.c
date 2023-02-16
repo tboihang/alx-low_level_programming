31c6c3335553772d7047958927cf4ad4e6a23d97
Tshepang <tboihang20@gmail.com>
2023-02-16 14:47:22 -0800

    Size of grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 /**
- * main - print out sizes of data types in C
- * code by tboihang
+ * main - print the size of various types
  * Return: 0
 */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-
-	printf("Size of a char: %lu byte(s)\n",(unsigned)sizeof(a));
-	printf("Size of an int: %lu byte(s)\n",(unsigned)sizeof(b));
-	printf("Size of a long int: %lu byte(s)\n",(unsigned)sizeof(c));
-	printf("Size of a long long int: %lu byte(s)\n",(unsigned)sizeof(d));
-	printf("Size of a float: %lu byte(s)\n", (unsigned)sizeof(e));
-	return (0)
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
