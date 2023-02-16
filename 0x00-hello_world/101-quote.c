9409d70d55db7fce61c0d670592537eeba0a35b8
Tshepang <tboihang20@gmail.com>
2023-02-16 15:07:36 -0800

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - prints C program
- *
- * Return 1
+ * main - print exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19
+ * Return:0
 */
-int mai(void)
+int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
