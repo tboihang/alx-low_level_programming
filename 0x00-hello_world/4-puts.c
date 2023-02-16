1c57afab1b71366248a7259c2e86a9a01df709eb
Tshepang <tboihang20@gmail.com>
2023-02-16 12:41:12 -0800

    Put c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle"
+ * return: 0-if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
