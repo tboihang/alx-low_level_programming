49c4fb1ed9f30bfce8f8508840b39c1fd5778150
Tshepang <tboihang20@gmail.com>
2023-02-16 13:40:30 -0800

    Size of grandeur

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print out sizes of data types in C
+ * code by tboihang
+ * Return: 0
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+}
