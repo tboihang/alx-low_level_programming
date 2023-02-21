300c81380b4a684138b9caaf5316f30abdcac348
Tshepang <tboihang20@gmail.com>
2023-02-21 14:17:40 -0800

    I sometimes suffer from insomnia.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Write a function that prints the alphabet, in lowercase
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'n'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
