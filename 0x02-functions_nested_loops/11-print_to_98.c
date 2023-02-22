7ffad1daecd715e7f1ae1df8964d457e7f79d5f9
Tshepang <tboihang20@gmail.com>
2023-02-21 17:38:45 -0800

    10-add

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,16 +1,16 @@
 #include "main.h"
+
 #include <stdio.h>
 
 /**
  * print_to_98 - engrave all neutral numbers from n to 98
- * @n: number to began printing 
- * Return: 0.
+ * @n: number to began printing
+ * Return: 0
  */
 
 void print_to_98(int n)
 
 {
-
 	if (n <= 98)
 	{
 	for (; n <= 98; n++)
@@ -23,7 +23,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-	printf("%d, ", n);
+	printf("%d,", n);
 	}
 	}
 	}
