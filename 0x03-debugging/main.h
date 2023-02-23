1642fe0237b96f254d9c680be45124e7d2dde449
Tshepang <tboihang20@gmail.com>
2023-02-23 08:31:08 -0800

    0-main

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+#include <stdio>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int days, int years);
+
+#endif /* MAIN_H */
