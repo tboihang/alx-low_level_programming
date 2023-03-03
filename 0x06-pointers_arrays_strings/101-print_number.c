85b1eba69fe0f8befad035e0bc64cc1d7bbcf559
Tshepang <tboihang20@gmail.com>
2023-03-03 06:47:34 -0800

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * prints_number - Prints integer.
+ * @n: Integer to be printed
+ *
+ */
+
+void print_number(int n);
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+	_putchar('-');
+	num = -num;
+	}
+
+	if ((num / 10) > 0)
+	print_number(num / 10);
+	_putchar((num % 10) + '0');
+
+}
