f6a8dd83f85ea0796fa18b9975a9b293c38b2b36
Tshepang <tboihang20@gmail.com>
2023-03-13 10:53:48 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,47 +1,42 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
-* str_concat - concatenates two strings.
-* @s1: The first string to print.
-* @s2: The second string to print.
-* Return: The sum of two strings.
-*/
-
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
+ */
 char *str_concat(char *s1, char *s2)
 {
-	int a, b;
-	int i, j = 0;
-	char *p;
+	char *conct;
+	int i, ci;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
-	for (a = 0; s1[a] || s2[a]; a++)
-	{
-		b++;
-	}
+	i = ci = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (i + ci + 1));
 
-	p = malloc(sizeof(char) * b);
-
-	if (p == NULL)
-	{
+	if (conct == NULL)
 		return (NULL);
+	i = ci = 0;
+	while (s1[i] != '\0')
+	{
+		conct[i] = s1[i];
+		i++;
 	}
 
-	for (i = 0; s1[i]; i++)
+	while (s2[ci] != '\0')
 	{
-		p[j++] = s1[i];
+		conct[i] = s2[ci];
+		i++, ci++;
 	}
-	for (i = 0; s2[i]; i++)
-	{
-		p[j++] = s2[i];
-	}
-	return (p);
+	conct[i] = '\0';
+	return (conct);
 }
